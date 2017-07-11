707881298c2dc1e4b13142118d91199eaf7b311a
Ray <1905561110@qq.com>
2017-07-11 15:23:46 +0800

    change file structure

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #训练word2vec模型 先解析wiki语料xml成txt然后预处理文本然后训练模型
+mkdir ltp_data
 cd ./word2vec
 mkdir seg
-python traditional2Simple.py
-cd ../
-mkdir split
-python split_file.py
 python seg_wiki_data.py
-cd ./word2vec
 python train.py #word2vec模型文件在word2vec 的wiki_model
 
 #测试改进的TextRank模型
